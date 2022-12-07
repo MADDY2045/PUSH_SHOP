1eecc79fd803969c7b053420105a1ba577e0d344
MADDY2045 <madhavaneee08@gmail.com>
2022-12-07 20:30:37 +0530

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,6 @@ if (process.env.NODE_ENV === 'development') {
 //connect the DB
 connectDB();
 
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-
 //body parser
 app.use(express.json());
 
@@ -46,18 +43,25 @@ app.get('/api/config/razorpay', (req, res) =>
   })
 );
 
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-
-//Configuring for deployment
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.dirname.join(__dirname, '/frontend/build')));
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
-}
 
 app.listen(
   PORT,
